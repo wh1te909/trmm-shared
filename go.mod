e5699d36a79b8545c9555b1365eadb677e6dd005
wh1te909 <dcparsi@gmail.com>
2021-10-01 17:40:53 +0000

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wh1te909/trmm-shared
+
+go 1.17
---
+++ types.go
@@ -0,0 +1,34 @@
+package trmm
+
+type CheckInNats struct {
+	Agentid string `json:"agent_id"`
+	Version string `json:"version"`
+}
+
+type AgentInfoNats struct {
+	Agentid      string  `json:"agent_id"`
+	Username     string  `json:"logged_in_username"`
+	Hostname     string  `json:"hostname"`
+	OS           string  `json:"operating_system"`
+	Platform     string  `json:"plat"`
+	TotalRAM     float64 `json:"total_ram"`
+	BootTime     int64   `json:"boot_time"`
+	RebootNeeded bool    `json:"needs_reboot"`
+}
+
+type WinSvcNats struct {
+	Agentid string           `json:"agent_id"`
+	WinSvcs []WindowsService `json:"services"`
+}
+
+type WindowsService struct {
+	Name             string `json:"name"`
+	Status           string `json:"status"`
+	DisplayName      string `json:"display_name"`
+	BinPath          string `json:"binpath"`
+	Description      string `json:"description"`
+	Username         string `json:"username"`
+	PID              uint32 `json:"pid"`
+	StartType        string `json:"start_type"`
+	DelayedAutoStart bool   `json:"autodelay"`
+}
