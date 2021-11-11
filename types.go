6d7f8e4d0dcdc15ae32fb907b71d33616c8b3675
wh1te909 <dcparsi@gmail.com>
2021-11-11 19:31:54 +0000

    add software

--- types.go
+++ types.go
@@ -56,3 +56,19 @@ type PublicIPNats struct {
 	Agentid  string `json:"agent_id"`
 	PublicIP string `json:"public_ip"`
 }
+
+type WinSoftwareList struct {
+	Name        string `json:"name"`
+	Version     string `json:"version"`
+	Publisher   string `json:"publisher"`
+	InstallDate string `json:"install_date"`
+	Size        string `json:"size"`
+	Source      string `json:"source"`
+	Location    string `json:"location"`
+	Uninstall   string `json:"uninstall"`
+}
+
+type WinSoftwareNats struct {
+	Agentid  string            `json:"agent_id"`
+	Software []WinSoftwareList `json:"software"`
+}
